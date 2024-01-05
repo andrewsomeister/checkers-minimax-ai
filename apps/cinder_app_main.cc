e3f330b4541688f5a09b87446fb4eb7dde1dc871
Andrew Shusterman <48100886+andrewsomeister@users.noreply.github.com>
2024-01-04 21:52:53 -0500

    Update cinder_app_main.cc

--- apps/cinder_app_main.cc
+++ apps/cinder_app_main.cc
@@ -3,7 +3,7 @@
 using checkers::CheckersApp;
 
 void prepareSettings(CheckersApp::Settings* settings) {
-  settings->setResizable(false);
+  settings->setResizable(true);
 }
 
 // This line is a macro that expands into an "int main()" function.
